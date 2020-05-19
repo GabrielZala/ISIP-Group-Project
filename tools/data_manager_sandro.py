8fbdbd24e0cbc7e19a3f991f91cb5fa9f5b57754
GabrielZala <gabriel.zala@unifr.ch>
2020-05-20 00:47:19 +0200

    Restructuring + Edge Detection
    ###
    you need to recompute everyting first because of big ass pickles, so i'm not pushing them into our master
    ###
    
    old stuff recieved pro forma adjustments, more comments and implementation into main
    
    new is some edge detection stuff i'm trying out

---
+++ tools/data_manager_sandro.py
@@ -0,0 +1,88 @@
+import numpy as np
+import glob
+from imageio import imread
+import matplotlib.pyplot as plt
+
+
+def preprocess_data():
+
+    plt.rcParams['image.cmap'] = 'gray'
+
+    # load the data in a dictionary where each key is the patients ID and the first
+    # image is the pre_img and the second image is the post_img
+    image_path = glob.glob("./DATA/ID*/ID*.png")
+
+    data = {}  # this is our data dictionary
+    patient_IDs = []  # this list is used for indexing during the crop
+
+    for i, image in enumerate(image_path):
+
+        patient_ID = image[9:11]  # gets the patient id string
+
+        if patient_ID not in patient_IDs:
+            patient_IDs.append(patient_ID)  # build list of patient ID's
+
+        # read pictures into arrays
+        post_img = np.array(imread("DATA/ID" + patient_ID + "/ID" + patient_ID + "post.png"))
+        pre_img = np.array(imread("DATA/ID" + patient_ID + "/ID" + patient_ID + "pre.png"))
+
+        data[patient_ID] = [pre_img, post_img]  # add to dictionary
+
+    # Let the cropping begin
+    for patient in patient_IDs:
+        for i in range(len(data[patient])):  # i = [0, 1] -> pre_img, post_img
+
+            # Crop away the first 23 pixelrows of every picture
+            data[patient][i] = data[patient][i][23:, :]
+
+            # normalize each individual pic to again hold values from 0 to 255
+            # get the min and max values and the resulting range
+            min_val = np.min(data[patient][i])
+            max_val = np.max(data[patient][i])
+            val_range = max_val - min_val
+
+            # calculate new values
+            data[patient][i] = ((data[patient][i] - min_val) / val_range) * 255
+
+    return data
+
+
+"""# I found that the watermark always goes exactly down to the 23th pixel
+# with the following code:
+if 0:
+  for patient in patient_IDs:  
+    plt.imshow(data[patient][0][0:24, 0:50])
+    plt.show()
+    plt.imshow(data[patient][0][0:24, -50:-1])
+    plt.show()
+    plt.imshow(data[patient][1][0:24, 0:50])
+    plt.show()
+    plt.imshow(data[patient][1][0:24, -50:-1])
+    plt.show()"""
+
+"""Old style (pre and post individually):
+    # Crop away the first 23 pixelrows of every picture
+    data[patient][0] = data[patient][0][23:, :]
+    data[patient][1] = data[patient][1][23:, :]
+    ### normalize each individual pic to hold values from 0 to 255
+    # get the min and max values
+    min0 = np.min(data[patient][0])
+    max0 = np.max(data[patient][0])
+    min1 = np.min(data[patient][1])
+    max1 = np.max(data[patient][1])
+    # store the range, calculate new values and assign them
+    range0 = max0 - min0
+    range1 = max1 - min1
+    data[patient][0] = ((data[patient][0] - min0) / range0) * 255
+    data[patient][1] = ((data[patient][1] - min1) / range1) * 255
+    # Verify that all pictures have values spaning from 0 to 255
+    if 1:
+        print("\n  patient:", patient)
+        print("old pre:  " + str(min0) + " to " + str(max0))
+        print("old post: " + str(min1) + " to " + str(max1))  
+        min0 = np.min(data[patient][0])
+        max0 = np.max(data[patient][0])
+        min1 = np.min(data[patient][1])
+        max1 = np.max(data[patient][1])
+        print("new pre:  " + str(int(min0)) + " to " + str(int(max0)))
+        print("new post: " + str(int(min1)) + " to " + str(int(max1)))"""
---
+++ tools/image_utils_cuba.py
@@ -0,0 +1,69 @@
+from matplotlib import pyplot as plt
+from skimage import feature , color
+
+
+def edge_map(image, sigma):
+    # Returns the edge map of a given image.
+    #
+    # Inputs:
+    #   img: image of shape (n, m, 3) or (n, m)
+    #
+    # Outputs:
+    #   edges: the edge map of image
+
+    img_grayscale = color.rgb2gray(image)
+    img_edges = feature.canny(img_grayscale, sigma)
+
+    return img_edges
+
+
+def show(image_array):
+    plt.imshow(image_array)
+    plt.show()
+
+
+
+def plot_image_list(list_images):
+
+    # get how many images you want to plot horizontally
+    nCols = len(list_images)
+    nRows = 1
+
+    # commence plotting
+    for index_image, image in enumerate(list_images, 1):  # start at 1 because of shitty pyplot indexing
+        plt.subplot(nRows, nCols, index_image)  # open subplot space to plot to
+        plt.imshow(image)  # plot the image into subplot space
+
+    plt.show()  # show plotted images
+
+
+def test_sigmas(data, testing_range=range(10, 15, 1), pre_or_post=0, patient="03"):
+
+    sample_image = data[patient][pre_or_post]
+
+    list_image_edge_maps = []
+
+    for sigma in testing_range:
+        edge_map_current_sigma = edge_map(sample_image, sigma)
+        list_image_edge_maps.append(edge_map_current_sigma)
+
+    plot_image_list(list_image_edge_maps)
+
+
+def data_to_edges(dict_data, sigma_pre, sigma_post):
+
+    dict_data_edges = {}
+
+    for patient in dict_data:
+
+        # read images
+        image_pre = dict_data[patient][0]
+        image_post = dict_data[patient][1]
+
+        # create their respective edge maps
+        edge_map_pre = edge_map(image_pre, sigma_pre)
+        edge_map_post = edge_map(image_post, sigma_post)
+
+        dict_data_edges[patient] = [edge_map_pre, edge_map_post]  # add to edge dictionary
+
+    return dict_data_edges
